1756af6b9e8ed4c6d2b63fe4ba8f032794751c6a
VENICE <vsc.devph@gmail.com>
2024-11-26 15:23:25 +0800

    Reactivities Init Setup

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,30 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseAPIController
+    {
+        private readonly DataContext _dataContext;
+        public ActivitiesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+            
+        }
+
+        [HttpGet] //api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _dataContext.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] //api/activities/id
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _dataContext.Activities.FindAsync(id);
+        }
+
+    }
+}
